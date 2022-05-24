c0ba74c1c595cb1d30608c79fbf7550a228ffc92
Enis Necipoglu <enis.necipoglu@gmail.com>
2022-05-24 14:45:42 +0300

    Add MAUI project
    with command: `dotnet new maui-blazor -n AbpCommunityTalks.Maui`

---
+++ src/AbpCommunityTalks.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AbpCommunityTalks.Maui;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
