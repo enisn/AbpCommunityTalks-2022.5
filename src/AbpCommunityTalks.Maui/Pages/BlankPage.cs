284e93d618aff559e3eecc437d8a30e384e0ece9
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-06-02 14:51:50 +0300

    Add a blank page

---
+++ src/AbpCommunityTalks.Maui/Pages/BlankPage.cs
@@ -0,0 +1,15 @@
+﻿using Volo.Abp.DependencyInjection;
+
+namespace AbpCommunityTalks.Maui.Pages;
+public class BlankPage : ContentPage, ITransientDependency
+{
+    public BlankPage()
+    {
+        Content = new Label
+        {
+            VerticalOptions = LayoutOptions.Center,
+            HorizontalOptions = LayoutOptions.Center,
+            Text = "Coming Soon..."
+        };
+    }
+}
